7844e2a5aac7bad7ec30f906341069ad56202e77
zxs <ujjfvswc@gmail.com>
2018-03-15 15:51:43 +0800

    Update CaesarCipher.clj
    format code

--- CaesarCipher.clj
+++ CaesarCipher.clj
@@ -2,26 +2,27 @@
 ;;https://www.codewars.com/kata/second-variation-on-caesar-cipher/train/clojure
 (ns caesar2.core)
 (defn circle-ascii [intch min max]
-    (let [length (inc (- max min))]
+  (let [length (inc (- max min))]
     (cond  
-	  (> intch max) (circle-ascii  (- intch length) min max)
+      (> intch max) (circle-ascii  (- intch length) min max)
       (< intch min) (circle-ascii  (+ intch length) min max)
-	  :else (char intch))))
+      :else (char intch))))
 		   
 (defn shift-char [ch shift]
-  (cond (Character/isLowerCase ch) (circle-ascii (+ (int ch) shift) (int \a) (int \z))
-	    (Character/isUpperCase ch) (circle-ascii (+ (int ch) shift) (int \A) (int \Z))
-		:else ch))
+  (cond 
+    (Character/isLowerCase ch) (circle-ascii (+ (int ch) shift) (int \a) (int \z)) 
+    (Character/isUpperCase ch) (circle-ascii (+ (int ch) shift) (int \A) (int \Z))
+    :else ch))
 
 (defn upper-char [ch]
   (if (Character/isLowerCase ch)
     (char (+ (int ch) (- (int \A) (int \a))))
-	ch))
+    ch))
   
 (defn lower-char [ch]
   (if (Character/isUpperCase ch)
-  (char (+ (int ch) (- (int \a) (int \A))))
-  ch))
+    (char (+ (int ch) (- (int \a) (int \A))))
+    ch))
 	
 (defn get-rotate [shift ch]
   (str ch (shift-char ch shift)))
@@ -35,30 +36,31 @@
 	
 (defn encode [s shift]
   (apply str 
-    (concat (encode-first (first s) shift) 
-            (map (partial encode-normal shift) (rest s)))))
+    (concat 
+      (encode-first (first s) shift) 
+      (map (partial encode-normal shift) (rest s)))))
   
 (defn avg-size [count]
-(do (println count)
-  (if (zero? (mod count 5))
-    (/ count 5)
-    (inc (int (/ count 5))))))
+  (do (println count)
+    (if (zero? (mod count 5))
+      (/ count 5)
+      (inc (int (/ count 5))))))
 
 (defn encode-str [s shift]
   (let
     [rs (encode s shift)
-	 size (avg-size (count rs))]
-	 (do (print s)
-	(map 
-	  #(apply str %) 
-	  (partition-all size rs)))))
+    size (avg-size (count rs))]
+    (do (println s)
+      (map 
+        #(apply str %) 
+	(partition-all size rs)))))
 	  
   
 (defn decode-first [s shift] 
-	(subs s 2))
+  (subs s 2))
 
 (defn decode-lower-char [shift ch]
-    (shift-char ch (- shift)))
+  (shift-char ch (- shift)))
   
 (defn decode-lower [s shift]
   (apply str (map (partial decode-lower-char shift)s)))
@@ -68,6 +70,7 @@
   
 (defn mydecode [s shift] 
   (decode-str (clojure.string/join s) shift))
+
 (defn decode [s] (mydecode s 1))
 
 
